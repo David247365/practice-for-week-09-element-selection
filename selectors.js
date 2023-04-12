01d2d6fc66bb32c03c836cf0a2ec6c37af4369d8
David Soto <ds051128@gmail.com>
2023-04-12 06:27:49 -0500

    initial commit

--- selectors.js
+++ selectors.js
@@ -1,38 +1,58 @@
 const select = () => {
-    /* Write queries for each of the following */
+	/* Write queries for each of the following */
 
-    /* Section 1 */
-    // 1. Get all seeded fruit elements
-    // Your code here
-    
-    // 2. Get all seedless fruit elements
-    // Your code here
+	/* Section 1 */
+	// 1. Get all seeded fruit elements
+	// Your code here
+	const seededFruit = document.getElementsByClassName("seed");
+	console.log(seededFruit);
 
-    // 3. Get first seedless fruit element
-    // Your code here
+	// 2. Get all seedless fruit elements
+	// Your code here
+	const seedlessFruits = document.getElementsByClassName("seedless");
+	console.log(seedlessFruits);
 
-    /* Section 2 */
-    // 4. Get inner span with text "you"
-    // Your code here
+	// 3. Get first seedless fruit element
+	// Your code here
+	let firstSeedless = seedlessFruits[0];
+	console.log(firstSeedless);
 
-    // 5. Get all children of element "wrapper"
-    // Your code here
+	/* Section 2 */
+	// 4. Get inner span with text "you"
+	// Your code here
+	const span = document.getElementsByTagName("span");
+	console.log(span /*[0].innerHTML*/);
 
-    // 6. Get all odd number list items in the list
-    // Your code here
+	// 5. Get all children of element "wrapper"
+	// Your code here
+	let wrapper = document.getElementById("wrapper");
+	console.log(wrapper);
 
-    // 7. Get all even number list items in the list
-    // Your code here
+	// 6. Get all odd number list items in the list
+	// Your code here
+	const odd = document.getElementsByClassName("odd");
+	console.log(odd);
 
-    /* Section 3 */
-    // 8. Get all tech companies without a class name
-    // Your code here
+	// 7. Get all even number list items in the list
+	// Your code here
+	const even = document.querySelectorAll("ol :nth-child(even)");
+	console.log(even);
 
-    // 9. Get "Amazon" list element
-    // Your code here
+	/* Section 3 */
+	// 8. Get all tech companies without a class name
+	// Your code here
+	const noClass = document.querySelector("a:not([class])");
+	console.log(noClass);
 
-    // 10. Get all unicorn list elements (not the image element)
-    // Your code here
-}
+	// 9. Get "Amazon" list element
+	// Your code here
+	const shopping = document.querySelector("a.shopping");
+	console.log(shopping);
+
+	// 10. Get all unicorn list elements (not the image element)
+	// Your code here
+	const unicorn = document.querySelectorAll("li > img.unicorn");
+	console.log(unicorn);
+};
 
 window.onload = select;
